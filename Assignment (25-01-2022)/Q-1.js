52febab67aa71dd5ef78b82354bcab2d26300f59
pawan kumar sharma <pawan1995219@gmail.com>
2022-01-26 16:53:02 +0530

    Add files via upload

---
+++ Assignment (25-01-2022)/Q-1.js
@@ -0,0 +1,13 @@
+var a=30;
+if(a>10)
+{
+console.log("The number is greater than to 10");
+}
+else if(a==10)
+{
+console.log("The number is equal to 10");
+}
+else
+{
+console.log("The number is less than 10");
+}
---
+++ Assignment (25-01-2022)/Q-2.js
@@ -0,0 +1,16 @@
+var a=10;
+var b=30;
+var c=63;
+
+if(a>b && a>c)
+{
+console.log("a is greater");
+}
+else if(b>a && b>c)
+{
+console.log("b is greater");
+}
+else
+{
+console.log("c is greater");
+}
