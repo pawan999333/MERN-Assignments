d6d3827f64151322410b92652dadbb13568004b2
pawan kumar sharma <pawan1995219@gmail.com>
2022-01-27 19:49:35 +0530

    Add files via upload

---
+++ Assignment-(26-01-2022)/Q-2/index.js
@@ -0,0 +1,8 @@
+var charr=prompt("Enter the character");
+if(charr=='a' || charr=='e' || charr=='i' || charr=='o' || charr=='u'||charr=='A' || charr=='E'|| charr=='I' ||charr=='O'||charr=='U')
+{
+alert("character is vowel");
+}
+else{
+alert("character is consonent");
+}
