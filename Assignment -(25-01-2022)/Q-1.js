2fe88ac91bc4bf6f8c19bc4d1a738f2c33d43961
pawan kumar sharma <pawan1995219@gmail.com>
2022-02-06 14:11:31 +0530

    Add files via upload

---
+++ Assignment -(25-01-2022)/Q-1.js
@@ -0,0 +1,15 @@
+//write a program to check whether the number is greater or equal to 10 or not.
+
+var a=30;
+if(a>10)
+{
+console.log("The number is greater than to 10");
+}
+else if(a==10)
+{
+console.log("The number is equal to 10");
+}
+else
+{
+console.log("The number is less than 10");
+}
